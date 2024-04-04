a64aa981df5e3f576cd4f2b375926eb57c5dabba
tompayfit <108389751+tompayfit@users.noreply.github.com>
2024-04-04 09:07:24 +0200

    card updated

--- src/components/SpotCard.jsx
+++ src/components/SpotCard.jsx
@@ -8,41 +8,25 @@ import { AiFillLike } from "react-icons/ai";
 import { FaHouseLaptop } from "react-icons/fa6";
 import { RiComputerFill } from "react-icons/ri";
 import { BiDollar } from "react-icons/bi";
+import { GiWaveSurfer } from "react-icons/gi";
 
 function SpotCard({ id }) {
   const spot = useSelector(selectSpots)[id];
 
   return (
     <>
-      <Card size="2" style={{ maxWidth: 240 }}>
-        <Inset clip="padding-box" side="top" pb="current">
-          <Link to={`/${spot.id}`} name={spot.name}>
-            <Text>{spot.name}</Text>
-            <Text as="p" size="4">
-              {spot.country}
-            </Text>
-          </Link>
-
-          <img
-            src={spot.image}
-            alt="Bold typography"
-            style={{
-              display: "block",
-              objectFit: "cover",
-              width: "100%",
-              height: 140,
-              backgroundColor: "var(--gray-5)",
-            }}
-          />
-        </Inset>
-        <Text as="p" size="3">
-          <Strong>Surfing level : </Strong> {spot.level}
-        </Text>
-      </Card>
+      
 
       <Card>
-        <Container>
-          <Grid container  spacing={2}>
+        <Container sx={{
+        backgroundImage: `url(${spot.image})`, // Ensure the path is correct
+        height: '385px',
+        backgroundSize: 'cover', // Adjust the size of the background image
+        backgroundPosition: 'center', // Center the background image
+        backgroundRepeat: 'no-repeat',
+        
+      }}>
+          <Grid container spacing={2}>
             <Grid item xs={9}>
               <Link to={`/${spot.id}`} name={spot.name}>
                 <Typography variant="h3" align="left">
@@ -59,6 +43,7 @@ function SpotCard({ id }) {
                 {[...Array(5)].map((_, index) => {
                   return (
                     <FaWifi
+                      key={index}
                       size={20}
                       color={index <= spot.wifiQuality ? "blue" : "grey"}
                     ></FaWifi>
@@ -84,12 +69,45 @@ function SpotCard({ id }) {
             </Grid>
 
             <Grid item xs={9}>
-              <Box align="left">Surf Season</Box>
-              <Box align="left">Level</Box>
+              <Box align="left" sx={{ display: "flex", alignItems: "center", gap: "0.2rem" }}>
+                {[
+                  "January",
+                  "Febuary",
+                  "March",
+                  "April",
+                  "May",
+                  "June",
+                  "July",
+                  "August",
+                  "Septembre",
+                  "October",
+                  "November",
+                  "December",
+                ].map((month, index) => {
+                  if (spot.surfSeason.includes(month)) {
+                    return <Typography key={index}>{month.slice(0, 3)} </Typography>;
+                  }
+                })}
+              </Box>
+              <Box align="left">
+                <GiWaveSurfer
+                  size={15}
+                  color={spot.level.includes("Beginner") ? "blue" : "grey"}
+                />
+                <GiWaveSurfer
+                  size={17}
+                  color={spot.level.includes("Intermediate") ? "blue" : "grey"}
+                />
+                <GiWaveSurfer
+                  size={20}
+                  color={spot.level.includes("Advanced") ? "blue" : "grey"}
+                />
+              </Box>
               <Box align="left">
                 {[...Array(5)].map((_, index) => {
                   return (
                     <BiDollar
+                      key={index}
                       size={20}
                       color={index <= spot.lifeCost ? "blue" : "grey"}
                     ></BiDollar>
